b84187e50f8a45ff62820910e039b03cb68509bb
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-06 23:31:52 +0900

    Mod parameter field ArrayElement

--- src/main/scala/composition_and_inheritance/Element.scala
+++ src/main/scala/composition_and_inheritance/Element.scala
@@ -6,6 +6,9 @@ abstract class Element {
   def width: Int = if (height == 0) 0 else contents(0).length
 }
 
-class ArrayElement(conts: Array[String]) extends Element {
-  def contents: Array[String] = conts
-}
+//class ArrayElement(conts: Array[String]) extends Element {
+//  def contents: Array[String] = conts
+//}
+
+// parameter field version
+class ArrayElement(val contents: Array[String]) extends Element
