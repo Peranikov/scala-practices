8ea8a4a7387df11c384f1a1b439c499f20d4a7e9
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-09 23:47:27 +0900

    Imprement adove, beside

--- src/main/scala/composition_and_inheritance/Element.scala
+++ src/main/scala/composition_and_inheritance/Element.scala
@@ -4,6 +4,19 @@ abstract class Element {
   def contents: Array[String]
   def height: Int = contents.length
   def width: Int = if (height == 0) 0 else contents(0).length
+
+  def adove(that: Element): Element = {
+    new ArrayElement(this.contents ++ that.contents)
+  }
+
+  def beside(that: Element): Element = {
+    val contents = new Array[String](this.contents.length)
+
+    for (i <- 0 until this.contents.length)
+      contents(i) = this.contents(i) + that.contents(i)
+
+    new ArrayElement(contents)
+  }
 }
 
 //class ArrayElement(conts: Array[String]) extends Element {
