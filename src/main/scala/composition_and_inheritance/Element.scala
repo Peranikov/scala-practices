c3ceaf4bb2dbaf8da2b8babf80dce3befa00352f
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-10 16:48:06 +0900

    Change private Element subclasses

--- src/main/scala/composition_and_inheritance/Element.scala
+++ src/main/scala/composition_and_inheritance/Element.scala
@@ -22,6 +22,19 @@ abstract class Element {
 }
 
 object Element {
+  private class ArrayElement(val contents: Array[String]) extends Element
+
+  private class LineElement(s: String) extends Element {
+    val contents = Array(s)
+    override def width = s.length
+    override def height = 1
+  }
+
+  private class UniformElement(ch: Char, override val width: Int, override val height: Int) extends Element {
+    private val line = ch.toString * width
+    def contents = Array.fill(height)(line)
+  }
+
   def elem(contents: Array[String]): Element =
     new ArrayElement(contents)
 
@@ -32,27 +45,9 @@ object Element {
     new LineElement(line)
 }
 
-//class ArrayElement(conts: Array[String]) extends Element {
-//  def contents: Array[String] = conts
-//}
-
-// parameter field version
-class ArrayElement(val contents: Array[String]) extends Element
-
-class LineElement(s: String) extends Element {
-  val contents = Array(s)
-  override def width = s.length
-  override def height = 1
-}
-
-class UniformElement(ch: Char, override val width: Int, override val height: Int) extends Element {
-  private val line = ch.toString * width
-  def contents = Array.fill(height)(line)
-}
-
 object ElementTest extends App {
-  val e1: Element = new ArrayElement(Array("hello", "world"))
+  val e1: Element = Element.elem(Array("hello", "world"))
   val ae: Element = Element.elem("hello")
   val e2: Element = ae
-  val e3: Element = new UniformElement('x', 2, 3)
+  val e3: Element = Element.elem('x', 2, 3)
 }
