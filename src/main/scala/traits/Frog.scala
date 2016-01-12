87f43af2d6e49b934128d30215749e6dd6eb7baf
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-12 23:35:47 +0900

    Add Frog class

---
+++ src/main/scala/traits/Frog.scala
@@ -0,0 +1,10 @@
+package traits
+
+class Animal
+
+class Frog extends Animal with Philosophical {
+  override def toString = "green"
+  override def philosophize(): Unit = {
+    println("It ain't easy being " + toString + "!")
+  }
+}
