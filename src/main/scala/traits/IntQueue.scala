5480e7c5772e72c38dc80898e8b4246b961926b8
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-13 23:39:29 +0900

    Add MixInSample

--- src/main/scala/traits/IntQueue.scala
+++ src/main/scala/traits/IntQueue.scala
@@ -14,3 +14,40 @@ class BasicIntQueue extends IntQueue {
 
   override def put(x: Int) { buf += x }
 }
+
+trait Doubling extends IntQueue {
+  abstract override def put(x: Int) { super.put(2 * x) }
+}
+
+class MyQueue extends BasicIntQueue with Doubling
+
+object MyQueueSample extends App {
+  val queue = new MyQueue
+  queue.put(10)
+  println(queue.get()) // 20
+}
+
+trait Incrementing extends IntQueue {
+  abstract override def put(x: Int) { super.put(x + 1) }
+}
+
+trait Filtering extends IntQueue {
+  abstract override def put(x: Int) {
+    if (x >= 0) super.put(x)
+  }
+}
+
+object MixInSample extends App {
+  val queue = (new BasicIntQueue with Incrementing with Filtering)
+  queue.put(-1); queue.put(0); queue.put(1);
+  println(queue.get()) // 1
+  println(queue.get()) // 2
+}
+
+object MixInSample2 extends App {
+  val queue = (new BasicIntQueue with Filtering with Incrementing)
+  queue.put(-1); queue.put(0); queue.put(1);
+  println(queue.get()) // 0
+  println(queue.get()) // 1
+  println(queue.get()) // 2
+}
