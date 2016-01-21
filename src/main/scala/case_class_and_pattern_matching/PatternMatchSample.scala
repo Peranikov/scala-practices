6c7413b8e73ce8c85ada547df3c8af3a24be281c
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-21 23:46:18 +0900

    Add constructor pettern, sequence pettern

--- src/main/scala/case_class_and_pattern_matching/PatternMatchSample.scala
+++ src/main/scala/case_class_and_pattern_matching/PatternMatchSample.scala
@@ -1,6 +1,9 @@
 package case_class_and_pattern_matching
 import math.{E, Pi}
 
+case class UserId(id: Int)
+case class User(id: UserId, age: Int, name: String)
+
 object PatternMatchSample extends App {
   println(
     E match {
@@ -23,4 +26,27 @@ object PatternMatchSample extends App {
       case _ => "OK" // unreachable code
     }
   ) // "OK"
+
+  val user = User(UserId(1), 20, "Jan")
+  println(
+    user match {
+      case User(UserId(1), 20, "Jan") => "found it."
+      case _ => "not found"
+    }
+  ) // "found it."
+
+  val list = List(0, 1, 2)
+  println(
+    list match {
+      case List(0, _, _) => "match List "
+      case _ => "not match List"
+    }
+  )
+
+  println(
+    list match {
+      case List(0, _*) => "match List "
+      case _ => "not match List"
+    }
+  )
 }
