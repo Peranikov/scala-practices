bf3e7b5e11d4c0ba4ab047f7c1fda812582e197a
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-22 23:23:58 +0900

    Type Pettern Match

--- src/main/scala/case_class_and_pattern_matching/PatternMatchSample.scala
+++ src/main/scala/case_class_and_pattern_matching/PatternMatchSample.scala
@@ -49,4 +49,14 @@ object PatternMatchSample extends App {
       case _ => "not match List"
     }
   )
+
+  def generalSize(x: Any) = x match {
+    case s: String => s.length
+    case m: Map[_, _] => m.size
+    case _ => -1
+  }
+
+  println(generalSize("abc"))
+  println(generalSize(Map(1 -> "a", 2 -> "b")))
+  println(generalSize(math.Pi))
 }
