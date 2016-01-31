90f8d852328c2f639ccd5811f11b50d1e312e991
Yuto Matsukubo <matsukubo@socket.co.jp>
2016-01-31 23:37:50 +0900

    Add ListSample

---
+++ src/main/scala/working_with_lists/ListSample.scala
@@ -0,0 +1,13 @@
+package working_with_lists
+
+object ListSample extends App {
+  // list concatenation
+  println(List(1, 2) ::: List(3, 4, 5))
+
+  def append[T](xs: List[T], ys: List[T]): List[T] = xs match {
+    case List() => ys
+    case x :: xs1 => x :: append(xs1, ys)
+  }
+
+  println(append(List.empty, List(1,2,3)))
+}
